1c75a6eac3b8e3a3198d1717455a5d55f65f72fa
Mike Starr <imhs@protonmail.com>
2022-04-10 18:27:55 -0700

    Fixed rendering errors in coinData component. Added optional chaining to everything

--- client/src/components/CoinData.js
+++ client/src/components/CoinData.js
@@ -5,19 +5,28 @@ const CoinStyles = styled.div`
   margin: auto;
   padding: 1rem;
   margin-top: 3rem;
-  border-top: 1px solid red;
+  /* border-top: 1px solid red; */
+  text-align: left;
 `;
 
 const Headline = styled.h1`
-  text-align: left;
+  display: flex;
+  align-items: center;
+  text-align: justify;
+  margin: 0;
   margin-top: 0.5em;
   margin-bottom: 0.5em;
 `;
 
 const Image = styled.img`
-  display: flex;
-
   align-items: center;
+  margin-right: 0.25em;
+`;
+
+const Website = styled.a`
+  background: lightyellow;
+  padding: 0.25em;
+  text-decoration: none;
 `;
 
 const Table = styled.table`
@@ -25,8 +34,8 @@ const Table = styled.table`
     text-align: left;
   }
 
-  &:td {
-    border-top: none;
+  &:tr {
+    border: none;
   }
 `;
 
@@ -93,95 +102,102 @@ function CoinData({ coin }) {
   }
 
   return (
-    <>
-      <CoinStyles>
-        <Headline>
-          <Image src={fullCoinData.image?.small} width="40px" />{" "}
-          {fullCoinData?.name} ({fullCoinData?.symbol?.toUpperCase()})
-        </Headline>
-        {/* <h3>
-          ${fullCoinData?.market_data.current_price.usd.toLocaleString()}{" "}
-          <span>
-            {fullCoinData?.market_data.price_change_percentage_24h.toFixed(1) >
-            0
-              ? "📈"
-              : "📉"}
-            {fullCoinData?.market_data.price_change_percentage_24h.toFixed(1)}%
-          </span>
-        </h3>
-        <br />
-        <h4>Rank: {fullCoinData?.market_data.market_cap_rank}</h4> */}
+    <CoinStyles>
+      <Headline>
+        <Image src={fullCoinData.image?.small} width="40px" />
+        {fullCoinData?.name} ({fullCoinData?.symbol?.toUpperCase()})
+      </Headline>
+      <h3>
+        ${fullCoinData?.market_data?.current_price.usd.toLocaleString()}{" "}
+        <span>
+          {fullCoinData?.market_data?.price_change_percentage_24h.toFixed(1) > 0
+            ? "📈"
+            : "📉"}
+          {fullCoinData?.market_data?.price_change_percentage_24h.toFixed(1)}%
+        </span>
+      </h3>
+      <br />
+      <h4>Rank: {fullCoinData?.market_data?.market_cap_rank}</h4>
+      <br />
+      <p>
+        Website:{" "}
+        <Website>
+          <a href={fullCoinData?.links?.homepage[0]}>
+            {fullCoinData?.links?.homepage[0]}
+          </a>
+        </Website>
+      </p>
 
-        <table>
-          {/* <tr>
+      <table>
+        <tbody>
+          <tr>
             <td>
               24h High: $
-              {fullCoinData?.market_data.high_24h.usd.toLocaleString()}
+              {fullCoinData?.market_data?.high_24h.usd.toLocaleString()}
             </td>
             <td>
-              24h Low: ${fullCoinData?.market_data.low_24h.usd.toLocaleString()}
+              24h Low: $
+              {fullCoinData?.market_data?.low_24h.usd.toLocaleString()}
             </td>
           </tr>
-
           <tr>
             <td>
               Price Change 24h: $
-              {fullCoinData?.market_data.price_change_24h.toLocaleString()}
+              {fullCoinData?.market_data?.price_change_24h?.toLocaleString()}
             </td>
             <td>
               Price Change % 24h:{" "}
-              {fullCoinData?.market_data.price_change_percentage_24h.toLocaleString()}
+              {fullCoinData?.market_data?.price_change_percentage_24h?.toLocaleString()}
               %
             </td>
           </tr>
           <tr>
             <td>
-              ATH: ${fullCoinData?.market_data.ath.usd.toLocaleString()} (
+              ATH: ${fullCoinData?.market_data?.ath.usd?.toLocaleString()} (
               {displayDate(
-                fullCoinData?.market_data.ath_date.usd.toLocaleString()
+                fullCoinData?.market_data?.ath_date?.usd?.toLocaleString()
               )}
               )
             </td>
             <td>
               ATH % change:{" "}
-              {fullCoinData?.market_data.ath_change_percentage.usd.toLocaleString()}
+              {fullCoinData?.market_data?.ath_change_percentage?.usd?.toLocaleString()}
               %
             </td>
           </tr>
-
           <tr>
             <td>
               Market Cap: $
-              {fullCoinData?.market_data.market_cap.usd.toLocaleString()}
+              {fullCoinData?.market_data?.market_cap?.usd?.toLocaleString()}
             </td>
             <td>
               Market Cap Change 24h: $
-              {fullCoinData?.market_data.market_cap_change_24h.toLocaleString()}
+              {fullCoinData?.market_data?.market_cap_change_24h?.toLocaleString()}
             </td>
           </tr>
           <tr>
             <td>
               Total Supply:{" "}
-              {fullCoinData?.market_data.total_supply.toLocaleString()}
+              {fullCoinData?.market_data?.total_supply?.toLocaleString()}
             </td>
             <td>
               Circulating Supply:{" "}
-              {fullCoinData?.market_data.circulating_supply.toLocaleString()}
+              {fullCoinData?.market_data?.circulating_supply?.toLocaleString()}
             </td>
           </tr>
           <tr>
             <td>
               Total Volume: $
-              {fullCoinData?.market_data.total_volume.toLocaleString()}
+              {fullCoinData?.market_data?.total_volume.usd?.toLocaleString()}
             </td>
             <td>
               Fully diluted valuation: $
-              {fullCoinData?.market_data.fully_diluted_valuation.toLocaleString()}
+              {fullCoinData?.market_data?.fully_diluted_valuation?.usd?.toLocaleString()}
             </td>
-          </tr> */}
-        </table>
-      </CoinStyles>
-    </>
+          </tr>
+        </tbody>
+      </table>
+    </CoinStyles>
   );
 }
 
